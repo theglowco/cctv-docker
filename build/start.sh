6c118c1db40879f4bbf35ef0135b13071e3bbbd5
Phil Ashton <phil@glow.co.uk>
2022-09-16 14:53:54 +0100

    1st commit

---
+++ build/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# mjpeg
+#/usr/bin/vlc --intf dummy rtsp://$CAMERAUSER:$CAMERAPASSWORD@$CAMERAIP:$CAMERAPORT$CAMERASTREAM --sout '#transcode{vcodec=mjpg,scale=1.0,acodec=none}:standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8080/}'
+
+# hls 
+sleep 5
+cp index.html ./html/index.html
+/usr/bin/vlc --intf dummy rtsp://$CAMERA_USER:$CAMERA_PASSWORD@$CAMERA_IP:$CAMERA_PORT/Streaming/channels/$CAMERA_STREAM --sout '#std{access=livehttp{seglen=$SEGLEN,delsegs=true,numsegs=$NUMSEGS,index=/home/streamer/html/camera'"$CAMERA_NUMBER"'.m3u8,index-url=camera'"$CAMERA_NUMBER"'-########.ts},mux=ts{use-key-frames},dst=/home/streamer/html/camera'"$CAMERA_NUMBER"'-########.ts}' vlc://quit
